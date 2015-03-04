7118bc13a6aa98e5a959adeb99400516a2a10434
Stephen Settle <ssettle3@gmail.com>
2015-03-04 14:55:36 -0500

    Profile Page lists your submitted beers

--- app/scripts/profile/profile.controller.js
+++ app/scripts/profile/profile.controller.js
@@ -7,11 +7,22 @@
 	.controller('ProfileController', ['$scope', 'UserFactory', 'BeerFactory', 
 		function ($scope, UserFactory, BeerFactory){
 
+			//Display Your Email
 			var user = UserFactory.user();
 			if(user){
 				$scope.userProfile = user.username;
 			}
 
+			// Display Your Beers
+			BeerFactory.fetch().success( function (data){
+				var a = data.results;
+				var b = a.filter( function (beer){
+					if(beer.user.username === user.username){
+						return beer
+					}
+				});
+				$scope.beerCol = b;
+			});
 
 		}
 
